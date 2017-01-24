832b6bf3e70a6aa4bef921692928e80df6592f3e
Raven856 <ralphv130@gmail.com>
2017-01-24 14:09:51 -0500

    first

--- Evaluator/Form1.Designer.cs
+++ Evaluator/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblHead = new System.Windows.Forms.Label();
+            this.txtExpression = new System.Windows.Forms.TextBox();
+            this.btnEvaluate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(106, 90);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(87, 23);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lblHead
+            // 
+            this.lblHead.AutoSize = true;
+            this.lblHead.Location = new System.Drawing.Point(103, 9);
+            this.lblHead.Name = "lblHead";
+            this.lblHead.Size = new System.Drawing.Size(100, 13);
+            this.lblHead.TabIndex = 1;
+            this.lblHead.Text = "Enter an expression";
+            // 
+            // txtExpression
+            // 
+            this.txtExpression.Location = new System.Drawing.Point(12, 35);
+            this.txtExpression.Name = "txtExpression";
+            this.txtExpression.Size = new System.Drawing.Size(267, 20);
+            this.txtExpression.TabIndex = 1;
+            // 
+            // btnEvaluate
+            // 
+            this.btnEvaluate.Location = new System.Drawing.Point(106, 61);
+            this.btnEvaluate.Name = "btnEvaluate";
+            this.btnEvaluate.Size = new System.Drawing.Size(87, 23);
+            this.btnEvaluate.TabIndex = 0;
+            this.btnEvaluate.Text = "Evaluate";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            this.btnEvaluate.Click += new System.EventHandler(this.btnEvaluate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 121);
+            this.Controls.Add(this.txtExpression);
+            this.Controls.Add(this.lblHead);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.btnBack);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblHead;
+        private System.Windows.Forms.TextBox txtExpression;
+        private System.Windows.Forms.Button btnEvaluate;
     }
 }
 
--- Evaluator/Form1.cs
+++ Evaluator/Form1.cs
@@ -7,14 +7,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
 namespace Evaluator
 {
+
     public partial class Form1 : Form
     {
+        public string type = "in";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            //go back
+        }
+
+        private void btnEvaluate_Click(object sender, EventArgs e)
+        {
+            var value = new double();
+            var nums = new Stack<double>();
+            var ops = new Stack<Char>();
+            var expression = txtExpression.Text;
+            var chars = expression.ToCharArray();
+            if (type == "post") {
+                //Goes through every character in the expression
+                foreach (char kar in chars) {
+                    //checks for a space
+                    if (kar == ' ')
+                    {
+                        continue;
+                    } else
+                    //checks to see if character is a number or an operator
+                    {
+                        if (kar >= 0 && kar <= 9)
+                        {
+                            nums.Push(kar);
+                        }
+                        else
+                        {
+                            ops.Push(kar);
+                        }
+                    }
+
+                }
+
+            }
+            else if (type == "pre") {
+                var stack = new Stack<Char>();
+
+            }
+            else //Infix
+            {
+                //Goes through every character in the expression
+                foreach (char kar in chars)
+                {
+                    //checks for a space
+                    if (kar == ' ')
+                    {
+                        Debug.WriteLine("space found");
+                        continue;
+                    }
+                    else
+                    //checks to see if character is a number or an operator
+                    {
+                        //Number
+                        if (kar >= 0 && kar <= 9)
+                        {
+                            nums.Push(kar);
+                            Debug.WriteLine("pushed number kar to nums value: "+ nums.Peek());
+                        }
+                        else
+                        //Operator
+                        {   // + or -
+                            if (kar == '+' || kar == '-')
+                            {
+                                //
+                                if (ops.Peek() == '*') {
+                                    nums.Push(nums.Pop() * nums.Pop());
+                                }
+                                else if (ops.Peek() == '/')
+                                {
+                                    nums.Push(nums.Pop() / nums.Pop());
+                                }
+                                else
+                                {
+                                    ops.Push(kar);
+                                    Debug.WriteLine("pushed + or - ....  kar was: " + ops.Peek());
+                                }
+                            }
+                            // ^
+                            else if (kar == '^')
+                            {
+                                var pwr = nums.Pop();
+                                nums.Push( Math.Pow(nums.Pop(), pwr) );
+                            }
+                            // * or /
+                            else
+                            {
+                                ops.Push(kar);
+                            }
+                        }
+                    }//All Characters pushed    
+                
+                    while (ops.Count != 0) {
+                        double temp = 0;
+                        switch ( ops.Pop() ) {
+                            case '-':
+                                temp = nums.Pop();
+                                nums.Push(nums.Pop() - temp);
+                                break;
+                            case '+':
+                                nums.Push(nums.Pop() + nums.Pop());
+                                break;
+                            case '/':
+                                temp = nums.Pop();
+                                nums.Push(nums.Pop() / temp);
+                                break;
+                            case '*':
+                                nums.Push(nums.Pop() * temp);
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                    //remaining number is value
+                    value = nums.Pop();
+                    lblHead.Text = value.ToString();
+                }
+            } 
         }
     }
 }
